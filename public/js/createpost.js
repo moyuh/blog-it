522af7f801b54ff3dd8a97f5ac143e532261daf1
moyuh <mlroth94@gmail..com>
2022-09-24 22:29:44 -0600

    adds public  create post js

---
+++ public/js/createpost.js
@@ -0,0 +1,10 @@
+const newPostHandler = async (event) => {
+    event.preventDefault();
+
+    document.location.replace('/dashboard/new')
+    }
+
+
+document
+    .querySelector('.create-blogpost')
+    .addEventListener('submit', newPostHandler);
