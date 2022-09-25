0604b5dfd7349770e91a007e9e5e60784b1aa780
moyuh <mlroth94@gmail..com>
2022-09-24 22:04:01 -0600

    adds public comment js

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ const loginFormHandler = async (event) => {
     const password = document.querySelector('#password-login').value.trim();
   
     if (name && password) {
-      const response = await fetch('/api/users/', {
+      const response = await fetch('/api/users/login', {
         method: 'POST',
         body: JSON.stringify({ name, password }),
         headers: { 'Content-Type': 'application/json' },
